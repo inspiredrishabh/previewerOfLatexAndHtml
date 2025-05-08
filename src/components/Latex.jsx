9f8b298c877dd82472d0cd23e053a3c99bc2269b
inspiredrishabh <160398029+inspiredrishabh@users.noreply.github.com>
2025-05-08 10:54:29 +0530

    updateComponent

---
+++ src/components/Latex.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Latex = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Latex
---
+++ src/components/Xml.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Xml = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Xml
